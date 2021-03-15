d0414dcf33ca2fa561665a2a2c70241ad473e175
Greta <gretahayes19@gmail.com>
2021-03-15 16:19:47 -0400

    Heroku push beginning

--- app/views/api/users/show.json.jbuilder
+++ app/views/api/users/show.json.jbuilder
@@ -2,8 +2,3 @@ json.extract! @user, :id, :firstname, :lastname, :email, :created_at
 
 
 
-
-$.ajax({
-    method: "DELETE",
-    url: '/api/session',
-})
--- frontend/all_treks.jsx
+++ frontend/all_treks.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {signUp, login, logout} from './util/session_api_util'
 
 document.addEventListener("DOMContentLoaded", () => {
     let root = document.getElementById("root");
     ReactDOM.render(<h1>Welcome to AllTreks</h1>, root)
 })
+
+window.signUp = signUp;
+window.login = login;
+window.logout = logout;
