ffc3691cfc2a02a654cf4cedf1920bab055d4d49
Greta <gretahayes19@gmail.com>
2021-04-19 16:43:14 -0400

    added stars to index item

--- frontend/components/hikes/hike_index_item.jsx
+++ frontend/components/hikes/hike_index_item.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
 import { Link } from "react-router-dom";
+import ReactStars from "react-rating-stars-component";
+
 
 const HikeIndexItem = ({ hike, park, idx }) => {
+
+    const avgRating = hike.avg_rating
+
+    const starOps = {
+        size: 18,
+        isHalf: true,
+        edit: false,
+        value: avgRating,
+    };
+
     return (
 
         <Link to={`/hikes/${hike.id}`} className="hike-item">
@@ -9,7 +21,10 @@ const HikeIndexItem = ({ hike, park, idx }) => {
             <div className="hike-item-content">
                 <h3 className="header-text3">{`#${idx + 1} - ${hike.name}`}</h3>
                 <p>{park}</p>
-                <p><span className={`hike-difficulty ${hike.difficulty}`}>{hike.difficulty}</span></p>
+                <p><span className={`hike-difficulty ${hike.difficulty}`}>
+                    {hike.difficulty}</span>
+                    <ReactStars {...starOps} />
+                    </p>
                 <p>Length: {hike.distance} mi</p>
                 <p className="hike-description">{hike.description}</p>
             </div>
