4e52757e2c284588d210f08a68ced145ac9e7325
Greta <gretahayes19@gmail.com>
2021-04-21 12:06:54 -0400

    review formatting

--- frontend/components/reviews/reviews_index.jsx
+++ frontend/components/reviews/reviews_index.jsx
@@ -6,25 +6,31 @@ import ReactStars from "react-rating-stars-component";
 
 const ReviewIndex = ({ reviews }) => {
     const starOps = {
-        size: 20,
+        size: 17,
         isHalf: false,
         edit: false,
     };
+
+    const monthNames = ["January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"];
+    
 
     return (
         <ul className="review-index">
             {reviews.map((review, idx) => (
                 <li className="review-item" key={review.id}>
-                    <div className="review-header">
+                    <div className="review-pic-name">
                         <div className="profile-pic" >
                             < FontAwesomeIcon icon={faHiking} />
                         </div>
-                        {review.reviewer.firstname} {review.reviewer.lastname}
-                       <br/>
-                        <ReactStars value={review.rating} {...starOps} />
-
+                        <div className="review-header">
+                            <h3 className="header-text3">{review.reviewer.firstname} {review.reviewer.lastname}</h3>
+                            
+                            <ReactStars value={review.rating} {...starOps} />
+                        </div>
                     </div>
-                    <p>{review.review_text}</p>
+
+                    <p className="review-text">{review.review_text}</p>
                     
                 </li>
             ))}
