96b57199a611834af7add9efd89c11a95284c7f3
Greta <gretahayes19@gmail.com>
2021-04-21 20:45:43 -0400

    fixed home page

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -14,7 +14,7 @@ const Home = ({currentUser}) => {
 
             </div>
             <div className="homepage-sub">
-                <h2>100,000+ trails. 20 million explorers. Endless memories.</h2>
+                <h2>100,000+ trails. 20 million explorers. <br/> Endless memories.</h2>
                 <p>The beauty of nature doesn’t need to be hard to find. Our goal is simple - build the largest collection of hand-curated trail guides, so you can explore the outdoors with confidence. Anytime. Anywhere.</p>
                 {(!currentUser) ? (<Link to="/signup" className="sign-up-button-home">Sign up for free</Link>) : null }
             </div>
